b50caf312dfc940f4d4920ccf2d338597600a283
Катя <58567818+2mkat@users.noreply.github.com>
2020-12-12 00:58:57 +0700

    Update array_l.h

--- array_l.h
+++ array_l.h
@@ -9,22 +9,26 @@ private:
 
 public:
 
-    Array(std::size_t size = 1, std::size_t key = 1, int range = 10);
+    Array(std::size_t , std::size_t, int);
     Array(const int*, std::size_t);
+    explicit Array(std::size_t len = 0);
 
-    Array(Array& );
+    Array(const Array& );
     ~Array();
 
-    Array& operator=(Array);
+    Array& operator=(const Array& );
     int& operator[](std::size_t );
     bool Test() const;
-    bool operator==(Array&) const;
-    Array operator+(Array&);
+    bool operator==(const Array&) const;
+    Array operator+(const Array&);
+    size_t get_len() const;
+    size_t get_bit() const;
 
     void Shell_sort();
     void Shaker_sort();
     void Hoar_sort(int , int );
     void Bit_sort(int ,int ,int );
+    void Pyramidal_sort();
 
     friend std::istream& operator>> (std::istream&, Array&);
     friend std::ostream& operator<< (std::ostream&, Array&);
